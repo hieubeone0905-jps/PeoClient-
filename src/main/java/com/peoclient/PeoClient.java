4d798f49876be615757bd961fc00073bf0e10cde
hieubeone0905-jps <hieubeone0905@gmail.com>
2026-09-05 09:32:41 +0700

    ssssssssssssssss

--- src/main/java/com/peoclient/PeoClient.java
+++ src/main/java/com/peoclient/PeoClient.java
@@ -16,6 +16,7 @@ import com.peoclient.nuker.optimize.ClientStabilizer;
 import com.peoclient.nuker.optimize.LatencyCompensator;
 import com.peoclient.nuker.optimize.AutoReloadEnhancer;
 import com.peoclient.nuker.bypass.BypassPacketManager;
+import com.peoclient.diagnostic.DiagnosticRecorder;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
